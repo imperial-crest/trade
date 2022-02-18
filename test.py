a5ca4ab85182051b5e7bf7b22509d6a7e8e76e13
imperial-crest <99951384+imperial-crest@users.noreply.github.com>
2022-02-18 18:27:10 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,13 @@
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 18 17:18:37 2022
+
+@author: kz
+"""
+
+print( "これはテスト")
+print( 100 )
+print( [0,1,2])
+print('只今の時間は、%d時%d分です。' % (17,30) )
